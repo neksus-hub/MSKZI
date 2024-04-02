b41cc57570f402aac84c26174f0cbc36ca6bc963
Nikita Dubinin <nik.dubinin.01@mail.ru>
2024-04-02 10:16:33 +0300

    fix

--- PSP+mask_lab1.cpp
+++ PSP+mask_lab1.cpp
@@ -16,22 +16,22 @@ int main()
     char name[16] = {};
     cout << "Enter the ciphertext file name ";
     cin >> name;
-    cout << "Введите имя файла c открытым текстом ";
+    cout << "Enter a plaintext file name";
     cin >> Name;
     ofstream out(name, ios::binary);
     ifstream in(Name, ios::binary);
     while (in.read((char*)&data, sizeof(long long int))) {
                 for (int i = 0; i < 64; i++) {
                     gamma = gamma << 1;
-                    bit_gamma = ((((rlz1 >> 22) ^ (rlz1 >> 17)) & 0x1) ^ (((rlz2[0] >> 59) ^ (rlz2[0] >> 22)) & 0x1));
+                    bit_gamma = ((((rlz1 >> 13) ^ (rlz1 >> 12)) & 0x1) ^ (((rlz2[0] >> 39) ^ (rlz2[0] >> 32)) & 0x1));
                     gamma = gamma + bit_gamma;
                     rlz1 = rlz1 << 1;
-                    rlz1 = rlz1 + (((rlz1 >> 22) ^ (rlz1 >> 17)) & 0x1);   // Запись бита обратной связи
+                    rlz1 = rlz1 + (((rlz1 >> 13) ^ (rlz1 >> 12)) & 0x1);   // Запись бита обратной связи
                     buffer = (rlz2[1] >> 63) & 0x1;
                     rlz2[0] = rlz2[0] << 1;
                     rlz2[1] = rlz2[1] << 1;
                     rlz2[0] = rlz2[0] + buffer;
-                    rlz2[1] = rlz2[1] + (((rlz2[0] >> 59) ^ (rlz2[0] >> 22)) & 0x1);   // Запись бита обратной связи
+                    rlz2[1] = rlz2[1] + (((rlz2[0] >> 39) ^ (rlz2[0] >> 32)) & 0x1);   // Запись бита обратной связи
                 }
                 mask = data ^ gamma;
                 out.write((char*)&mask, sizeof(long long int));
